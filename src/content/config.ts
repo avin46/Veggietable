29e89834053798d33e5d6b6fb3ad7266550f381e
Karthik Avineni <karthik.avineni@gmail.com>
2026-01-13 23:39:23 +0530

    Fix blog grid with hero images

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,15 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
+
+const blog = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.date(),
+    heroImage: z.string().optional(),
+  }),
+});
 
 export const collections = {
-  blog: defineCollection({
-    type: 'content',
-    schema: z.object({
-      title: z.string(),
-      description: z.string(),
-      pubDate: z.date(),
-      heroImage: z.string(),
-    }),
-  }),
+  blog,
 };
